1d708f147dc7b6a108884c1a6be76031856b161d
TriMkort <maikol.stiven.16.1998@gmail.com>
2026-08-17 22:37:13 -0500

    AI ayuda a crear efectos visuales del menu creando un nuevo css y se crea en la carpeta data archivo js para los detalles

---
+++ src/data/menuItems.js
@@ -0,0 +1,110 @@
+// data/menuItems.js
+export const entradas = [
+        {
+            id: 1,
+            nombre: 'SOPA DE TOMATE Y STRACCIATELLA',
+            precio: '$18.000',
+            descripcion: 'Tomate asados a las brasas, pesto de almendras y queso hilado de búfala del Rosal, Cundinamarca.'
+        },
+        {
+            id: 2,
+            nombre: 'CREMA DE ALCACHOFAS',
+            precio: '$19.000',
+            descripcion: 'Corazones de alcachofa, jamón serrano y aceite de trufa'
+        },
+        {
+            id: 3,
+            nombre: 'CAYEYE CON CHICHARRÓN',
+            precio: '$21.000',
+            descripcion: 'Puré de maduro, suero costeño y chicharrones.'
+        },
+        {
+            id: 4,
+            nombre: 'TARTAR DE LOMO',
+            precio: '$29.000',
+            descripcion: 'Con yema curada, mostaza y pepinilos sobre hueso de tuétano y tostada de pan.'
+        },
+        {
+            id: 5,
+            nombre: 'CRUDO DE ATÚN',
+            precio: '$34.000',
+            descripcion: 'Con pepino europeo, agua chile de limón mandarino, ajonjolí de montes de María, yogurt griego y aceite de menta.'
+        },
+        {
+            id: 6,
+            nombre: 'CREMA DE CAMARÓN Y LANGOSTINO',
+            precio: '$28.000',
+            descripcion: 'Camarones cocinados en sofrito del Pacifico, con hierbas de azotea y leche de coco.'
+        },
+        {
+            id: 7,
+            nombre: 'CEVICHE CROCANTE',
+            precio: '$29.000',
+            descripcion: 'Camarones y calamar en tempura, pescado fresco en leche de tigre y aguacate ahumado.'
+        },
+        {
+            id: 8,
+            nombre: 'COCTEL PLAYERO',
+            precio: '$28.000',
+            descripcion: 'Clásico coctel de las playas del Caríbe en salsa rosada, cebolla, cilantro y crocantes de plátano.'
+        },
+    ]
+
+export const platoPrincipales = [
+        {
+                id: 9,
+                nombre: 'CHICHARRONES',
+                precio: '$33.000',
+                descripcion: 'Panceta de cerdo toteada al horno sobre guacamole y papas criollas.'
+            },
+        {
+                id: 10,
+                nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
+                precio: '$25.000',
+                descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+            },
+        {
+                id: 11,
+                nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
+                precio: '$25.000',
+                descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+            },
+        {
+                id: 12,
+                nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
+                precio: '$25.000',
+                descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+            },
+        {
+                id: 13,
+                nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
+                precio: '$25.000',
+                descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+            },
+        {
+                id: 14,
+                nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
+                precio: '$25.000',
+                descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+            },
+        {
+                id: 15,
+                nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
+                precio: '$25.000',
+                descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+            },
+        {
+                id: 16,
+                nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
+                precio: '$25.000',
+                descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+            }
+    ]
+export const pescadosMariscos = [
+        {
+            id: 17,
+            nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
+            precio: '$25.000',
+            descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+        },
+    ]
