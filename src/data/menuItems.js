661a5592f3caafbec5c8940c83d3a70885c68533
scott <scott@scott.kali.net>
2026-08-23 14:36:37 -0500

    Descripcion de carta al menu del restaurante

--- src/data/menuItems.js
+++ src/data/menuItems.js
@@ -65,37 +65,37 @@ export const platoPrincipales = [
             },
         {
                 id: 11,
-                nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
-                precio: '$25.000',
-                descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+                nombre: 'TOSTADAS DE ATÚN ROJO Y TAMARINDO',
+                precio: '$33.000',
+                descripcion: 'Tortillas de maíz crujientes con tartar de atún rojo, aguacate, mayonesa y salsa Valentina.'
             },
         {
                 id: 12,
-                nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
-                precio: '$25.000',
-                descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+                nombre: 'BURRATA',
+                precio: '$38.000',
+                descripcion: 'Tartar de de tomates, rúgula, sal de albahaca, peras rostizadas y marañones.'
             },
         {
                 id: 13,
-                nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
-                precio: '$25.000',
-                descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+                nombre: 'ALBÓNDIGAS DE CORDERO',
+                precio: '$29.000',
+                descripcion: 'En salsa española, queso paipa y tostadas de pan.'
             },
         {
                 id: 14,
-                nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
-                precio: '$25.000',
-                descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+                nombre: 'BRIE CROCANTE',
+                precio: '$38.000',
+                descripcion: 'Queso semi maduro envuelto en masa filo con mermelada de arándanos y uchuvas.'
             },
         {
                 id: 15,
-                nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
-                precio: '$25.000',
-                descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+                nombre: 'QUESO PAIPA AL HORNO',
+                precio: '$33.000',
+                descripcion: 'Con mermelada de tomate y acompañado de pan tostado.'
             },
         {
                 id: 16,
-                nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
+                nombre: '*****************',
                 precio: '$25.000',
                 descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
             }
@@ -103,8 +103,48 @@ export const platoPrincipales = [
 export const pescadosMariscos = [
         {
             id: 17,
-            nombre: 'TRUCHA CURADA CON VEGETALES ENCURTIDOS',
+            nombre: 'TILAPIA FRESCA',
+            precio: '46.000',
+            descripcion: 'Filete de Tilapia de 280gr, acompañada de ensalada fresca.'
+        },
+        {
+            id: 18,
+            nombre: 'SALMÓN AL ENELDO',
+            precio: '$57.000',
+            descripcion: 'Salmón asado de 200gr, con salsa cremosa de lima y eneldo,  acompañado de ensalada fresca.'
+        },
+        {
+            id: 19,
+            nombre: 'SALMON EN LECHE DE COCO Y CURRY',
             precio: '$25.000',
-            descripcion: 'Trucha arcoíris, pepinos y rábanos encurtidos con leche de tigre de toronja y limón mandarino, papa nativa y crema de queso.'
+            descripcion: 'Salmón asado de 200gr, finalizado en mantequilla de maní, curry amarillo y leche de coco, acompañado de papas con mantequilla'
+        },
+        {
+            id: 20,
+            nombre: 'ATÚN ROJO CON SALSA DE TAMARINDO',
+            precio: '$57.000',
+            descripcion: 'Lomo de Atún rojo de 200g, en reducción de tamarindo, tucupí y panela, acompañado de ensalada fresca.'
+        },
+        {
+            id: 21,
+            nombre: 'PIRARÚCU EN MANTEQUILLA DE LIMÓN',
+            precio: '$57.000',
+            descripcion: 'Filete de pescado amazónico de 200g, finalizado en manteuilla, vino blanco y un toque de limón, acompañado de puré de ñame y papa.'
+        },
+        {
+            id: 22,
+            nombre: 'CHICHARRÓN DE PIRARÚCU',
+            precio: '$48.000',
+            descripcion: 'Filete de Pirarúcu amazónico de 200g, rebosado y frito con ajillo cremoso, acompañado de puré de ñame y papa.'
+        },
+        {
+            id: 23,
+            nombre: 'CAZUELA DEL CARIBE',
+            precio: '$48.000',
+            descripcion: 'Inspiración del Caribe colombiano: mariscos frescos en fondo de langosta y langostino, leche de coco, acompañado de arroz y patacón.'
+        },
+        {
+            id: 24,
+            descripcion: 'Elige uno de nuestros acompañamientos: Papas con mantequilla de ajo y perejil, puré cremoso de ñame y papa, papa francesa, patacón, porción de aguacate, ensalada fresca, vegetales salteados (Berenjena, zanahoria baby, mini tomates, brocoli, cebolla) arroz frijol cabeza negra. *No incluye acompañamientos.'
         },
     ]
